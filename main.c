78847f903416c8a27d1714c0ebdcf4f427f1b2bc
Samuel-Yeek <syeek@my.wctc.edu>
2024-03-10 04:53:55 -0500

    Update main.c
    Now with player angled turning!

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <math.h>
+#define PI 3.1415926535
 
 
 //player position
 
 float playerx; // Player X axis
 float playery; // Player Y axis
-
+float pdx,pdy; //player delta x and y
+float playerangle; //playerangle
 
 
 void drawPlayer(){
@@ -18,8 +21,39 @@ void drawPlayer(){
 	glEnd();
 }
 
+//The map
+
+int mapX=8,mapY=8,mapS=64; // map X length, map Y length, and cube size
+int map[]=
+{
+	1,1,1,1,1,0,1,1,
+	1,0,0,0,1,0,0,1,
+	1,0,0,0,1,0,0,1,
+	1,0,0,0,1,0,1,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,1,1,1,1,1,1,1,
+};
 
 
+void drawMiniMap(){
+	int x,y,xo,yo;
+	for(y=0;y<mapY;y++)
+	{
+		for (x=0;x<mapX;x++)
+		{
+			if(map[y*mapX+x]==1){ glColor3f(1,1,1);} else{ glColor3f(0,0,0);}
+			xo=x*mapS; yo=y*mapS;
+			glBegin(GL_QUADS);
+			glVertex2i(xo+1     ,yo     +1);
+			glVertex2i(xo+1     ,yo+mapS-1);
+			glVertex2i(xo-1+mapS,yo+mapS-1);
+			glVertex2i(xo-1+mapS,yo     +1);
+			glEnd();
+			}
+		}	
+}
 
 
 
@@ -27,15 +61,16 @@ void drawPlayer(){
 void display()
 {
  	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ 	drawMiniMap();
  	drawPlayer();
  	glutSwapBuffers();
 }
 
 void buttons(unsigned char key,int x,int y){
-	if(key=='a'){ playerx-+5;}
-	if(key=='d'){ playerx+=5;}
-	if(key=='w'){ playery-=5;}
-	if(key=='s'){ playery+=5;}
+	if(key=='a'){ playerangle-=0.1; if(playerangle<0){playerangle+=2*PI;} pdx=cos(playerangle)*5; pdy=sin(playerangle)*5;}
+	if(key=='d'){ playerangle+=0.1; if(playerangle>2*PI){playerangle-=2*PI;} pdx=cos(playerangle)*5; pdy=sin(playerangle)*5;} //turn based on a hardcoded amount, will be adjustable later
+	if(key=='w'){ playerx+=pdx; playery+=pdy;}
+	if(key=='s'){ playerx-=pdx; playery-=pdy;}
 	glutPostRedisplay();
 }
 
